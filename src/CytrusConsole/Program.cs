6654ded4b37a9c19fc05c06102a38e47287f94a2
lucian.carata <calucian@yahoo.co.uk>
2009-06-09 01:43:23 +0300

    Code updates to match the latest binaries release, the DirectShowCameraSource works as expected. TODO: complete the callback function.

--- src/CytrusConsole/Program.cs
+++ src/CytrusConsole/Program.cs
@@ -11,8 +11,21 @@ namespace CytrusConsole
         static void Main(string[] args)
         {
             Class1 c=new Class1();
-
-            Console.Write(c.getU()+1);
+            Console.WriteLine("DEBUG: Choose one input device:");
+            int i = 1;
+            foreach(string s in c.cList){
+                Console.Write("\t"+i+". ");
+                Console.WriteLine(s);
+                i++;
+            }
+            Console.WriteLine("");
+            Console.Write("choice:>");
+            string answer = Console.ReadLine();
+            int u=Int32.Parse(answer);
+            c.selectCamera(u);
+            c.startCapture();
+            Console.ReadLine();
+            c.stopCapture();
         }
     }
 }
