9d7e07de933260f6d8d525070048bdfdbd376636
lucian.carata <calucian@yahoo.co.uk>
2009-06-17 01:08:28 +0300

    - Moved some header files in the include dir - GUI work (WPF)

---
+++ src/Cytrus/RecognisedObject.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace cytrus.managed
+{
+    public class RecognisedObject
+    {
+        public string ObjectName { get; set; }
+        public int RecognitionCertainty { get; set; }
+
+        public RecognisedObject(string name, int certainty)
+        {
+            ObjectName = name;
+            RecognitionCertainty = certainty;
+        }
+    }
+}
